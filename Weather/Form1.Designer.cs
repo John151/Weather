2de887b1c74a79af8628e7b48e3911c9eff71fba
JC <blahblahblah@email.com>
2020-02-20 12:06:36 -0600

    Add project files.

---
+++ Weather/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.btnGetWeather = new System.Windows.Forms.Button();
+            this.picWeather = new System.Windows.Forms.PictureBox();
+            this.lblWeather = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picWeather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "State:";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(70, 28);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(131, 20);
+            this.txtCity.TabIndex = 1;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(300, 28);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(88, 20);
+            this.txtState.TabIndex = 2;
+            // 
+            // btnGetWeather
+            // 
+            this.btnGetWeather.Location = new System.Drawing.Point(417, 26);
+            this.btnGetWeather.Name = "btnGetWeather";
+            this.btnGetWeather.Size = new System.Drawing.Size(115, 23);
+            this.btnGetWeather.TabIndex = 3;
+            this.btnGetWeather.Text = "Get Weather";
+            this.btnGetWeather.UseVisualStyleBackColor = true;
+            this.btnGetWeather.Click += new System.EventHandler(this.btnGetWeather_Click);
+            // 
+            // picWeather
+            // 
+            this.picWeather.Location = new System.Drawing.Point(12, 79);
+            this.picWeather.Name = "picWeather";
+            this.picWeather.Size = new System.Drawing.Size(520, 335);
+            this.picWeather.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picWeather.TabIndex = 5;
+            this.picWeather.TabStop = false;
+            // 
+            // lblWeather
+            // 
+            this.lblWeather.Location = new System.Drawing.Point(113, 116);
+            this.lblWeather.Name = "lblWeather";
+            this.lblWeather.Size = new System.Drawing.Size(340, 93);
+            this.lblWeather.TabIndex = 5;
+            this.lblWeather.Text = "Enter city and state to get weather";
+            this.lblWeather.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(457, 431);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGetWeather;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(544, 457);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblWeather);
+            this.Controls.Add(this.picWeather);
+            this.Controls.Add(this.btnGetWeather);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Current Weather";
+            ((System.ComponentModel.ISupportInitialize)(this.picWeather)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.Button btnGetWeather;
+        private System.Windows.Forms.PictureBox picWeather;
+        private System.Windows.Forms.Label lblWeather;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Weather/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+
+namespace Weather
+{
+    public partial class Form1 : Form
+    {
+        readonly string BaseUrl = "https://weather-csharp.herokuapp.com/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetWeather_Click(object sender, EventArgs e)
+        {
+            //disable button to prevent multiple requests before one is complete
+            btnGetWeather.Enabled = false;
+
+            //read the data from the TextBoxes
+            string city = txtCity.Text;
+            string state = txtState.Text;
+
+            //fetch current weather and display
+            string weather = GetWeatherText(city, state);
+            lblWeather.Text = weather;
+
+            //re-enable button for another request
+            btnGetWeather.Enabled = true;
+        }
+
+        private string GetWeatherText(string city, string state)
+        {
+            //use the format methof to make a string in the format
+            // http://weater-csharp.herokuapp.com/text?city=dallas&state=tx
+
+            string weatherTextUrl = String.Format("{0}text?city={1})&state={2}", BaseUrl, city, state);
+            Debug.WriteLine(weatherTextUrl); //message for developer
+            string weatherText; //left off here!! pick up here!!
+
+            using (WebClient client = new WebClient())
+            {
+                weatherText = client.DownloadString(weatherTextUrl);
+            }
+
+            return weatherText;
+        }
+
+    }
+}
